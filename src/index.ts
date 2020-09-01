f994d9f33956ac74cb892722b917f82d8cf4ef21
Gabriel Minossi <gabriel.minossi@gmail.com>
2020-09-01 20:55:40 -0300

    Adding TypeORM

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
-const { GraphQLServer } = require('graphql-yoga')
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
-}
-`
+createConnection().then(async connection => {
 
-// 2
-const resolvers = {
-  Query: {
-    info: () => `This is my new API`
-  }
-}
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await connection.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
 
-// 3
-const server = new GraphQLServer({
-  typeDefs,
-  resolvers,
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+    console.log("Loading users from the database...");
+    const users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
+
+    console.log("Here you can setup and run express/koa/any other framework.");
+
+}).catch(error => console.log(error));
