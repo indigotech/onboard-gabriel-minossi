1f195c0fc2be4309ad737209feea96a2bc2afb7d
Gabriel Minossi <gabriel.minossi@gmail.com>
2020-09-03 18:02:59 -0300

    Adding setup and teardown of a test user for login tests

--- test/index.ts
+++ test/index.ts
@@ -1,33 +1,59 @@
 import * as assert from 'assert';
 import 'reflect-metadata';
 import * as supertest from 'supertest';
-import { getRepository, Repository, createConnection } from "typeorm";
+import { createConnection, getRepository } from "typeorm";
 import { User } from "../src/entity/User";
 import { server } from '../src/index';
+import { toUnicode } from 'punycode';
 
-const url = 'http://localhost:4000';
+const url = 'http://localhost:4001';
 
 describe('GraphQL', () => {
   before(async () => {
-    try {
-      await createConnection('test');
-      server.start({port: 4001}, () => console.log(`Server is running on http://localhost:4001`));
-    } catch (error) {
-      error => console.error('Error connecting to databse: ' + error)
-    }
+    await createConnection('test');
+    server.start({ port: 4001 }, () => console.log(`Server is running on http://localhost:4001`));
   });
   describe('Login', () => {
     let request: supertest.SuperTest<supertest.Test>;
+    const testUser = {
+      name: "test",
+      email: "test-email@example.com",
+      password: "supersafe",
+      birthDate: "01-01-1970",
+      cpf: 28
+    };
+
+    const wrongCredentials = {
+      email: testUser.email.split("").reverse().join(""),
+      password: testUser.password.split("").reverse().join("")
+    }
+
     before(() => {
       request = supertest(url);
+
+      const userRepository = getRepository(User);
+      userRepository.save(testUser);
     });
 
-    const email = "batata";
-    const password = "pass";
-    it(`Successfully returns token for user with email "${email}" and password "${password}"`, (done) => {
+    after(async () => {
+      const userRepository = getRepository(User);
+      const users = await userRepository.find({
+        where: {
+          name: testUser.name,
+          email: testUser.email,
+          password: testUser.password,
+          birthDate: testUser.birthDate,
+          cpf: testUser.cpf
+        }
+      });
+      const user = users[users.length - 1];
+      userRepository.delete(user);
+    })
+
+    it(`Successfully returns token for user with correct credentials`, (done) => {
       request.post('/')
         .send({
-          query: `mutation { login(email: "${email}", password: "${password}") { token } }`
+          query: `mutation { login(email: "${testUser.email}", password: "${testUser.password}") { token } }`
         })
         .expect(200)
         .end((err, res) => {
@@ -37,11 +63,10 @@ describe('GraphQL', () => {
         })
     });
 
-    const wrongPassword = "ssap";
-    it(`Fails logging in for email "${email}" and password "${wrongPassword}"`, (done) => {
+    it(`Fails logging in for an existent email with a wrong password`, (done) => {
       request.post('/')
         .send({
-          query: `mutation { login(email: "${email}", password: "${wrongPassword}") { token } }`
+          query: `mutation { login(email: "${testUser.email}", password: "${wrongCredentials.password}") { token } }`
         })
         .expect(200)
         .end((err, res) => {
@@ -51,11 +76,10 @@ describe('GraphQL', () => {
         })
     });
 
-    const wrongEmail = "atatab";
-    it(`Fails logging in for email "${wrongEmail}" and password "${password}"`, (done) => {
+    it(`Fails logging in for an unexistent email and an existent password`, (done) => {
       request.post('/')
         .send({
-          query: `mutation { login(email: "${wrongEmail}", password: "${password}") { token } }`
+          query: `mutation { login(email: "${wrongCredentials.email}", password: "${testUser.password}") { token } }`
         })
         .expect(200)
         .end((err, res) => {
