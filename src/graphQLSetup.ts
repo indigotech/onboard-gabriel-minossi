41d1ab67bd32481a8f3cebc90f0d1a33ab00864c
Gabriel Minossi <gabriel.minossi@gmail.com>
2020-09-03 19:56:02 -0300

    Refactoring graphql setup to it's own file importing graphql server setup to src/index.ts and test/index.ts

---
+++ src/graphQLSetup.ts
@@ -0,0 +1,103 @@
+import { GraphQLServer } from "graphql-yoga";
+import * as jwt from 'jsonwebtoken';
+import { getRepository, Repository } from "typeorm";
+import { User } from "./entity/User";
+
+const JWT_SECRET = "SICRET"
+
+const typeDefs = `
+type Query {
+    hello: String
+}
+
+type Mutation {
+    login(email: String!, password: String!, rememberMe: Boolean): Login
+    createUser(user: UserInput!): User!
+}
+
+type User {
+    id: ID!
+    name: String!
+    email: String!
+    birthDate: String!
+    cpf: Int!
+}
+
+input UserInput {
+    name: String!
+    email: String!
+    password: String!
+    birthDate: String!
+    cpf: Int!
+}
+
+type Login {
+    user: User!
+    token: String!
+}
+`;
+
+const getVerification = async auth => {
+    if (!auth) { 
+        throw new jwt.JsonWebTokenError('you must be logged in!'); 
+    }
+
+    const token = auth.split('Bearer ')[1];
+    if (!token) { 
+        throw new jwt.JsonWebTokenError('you should provide a token!'); 
+    }
+
+    const verification = jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if (err) { 
+            throw new jwt.JsonWebTokenError('invalid token!'); 
+        }
+        return decoded;
+    });
+    return verification;
+};
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello!'
+    },
+    Mutation: {
+        login: async (_: any, { email, password, rememberMe }) => {
+            const userRepository: Repository<User> = getRepository(User);
+
+            let user: User;
+            console.log("Searching for user on the database...");
+            try {
+                user = await userRepository.findOneOrFail({ where: { email } });
+            } catch {
+                throw new Error('Invalid Credentials')
+            }
+            if (password !== user.password) {
+                throw new Error('Invalid Credentials')
+            } else {
+                const token = jwt.sign(
+                    { id: user.id },
+                    JWT_SECRET,
+                    { expiresIn: rememberMe ? "1w" : "1h" }
+                );
+                return ({ user, token, })
+            }
+        },
+
+        createUser: (_, { user }) => {
+            const userRepository = getRepository(User);
+            user = {
+                name: user.name,
+                email: user.email.toLowerCase(),
+                password: user.password,
+                birthDate: user.birthDate,
+                cpf: user.cpf,
+            }
+            return userRepository.save(user);
+        }
+    },
+};
+
+export const graphQLServer = new GraphQLServer({
+    typeDefs,
+    resolvers,
+});
--- test/index.ts
+++ test/index.ts
@@ -1,22 +1,28 @@
 import * as assert from 'assert';
+import { verify as verifyToken } from 'jsonwebtoken';
 import 'reflect-metadata';
 import * as supertest from 'supertest';
-import { createConnection, getRepository } from "typeorm";
+import { createConnection, Repository, createConnections, getConnection, getRepository } from "typeorm";
 import { User } from "../src/entity/User";
-import { server } from '../src/index';
-import { verify as verifyToken, JsonWebTokenError } from 'jsonwebtoken';
+import { graphQLServer } from '../src/graphQLSetup';
 
-const JWT_SECRET = "SICRET"
-
-const url = 'http://localhost:4001';
+const JWT_SECRET = "SICRET";
+const port = 4001;
+const url = 'http://localhost:' + port;
 
 describe('GraphQL', () => {
+
   before(async () => {
-    await createConnection('test');
-    server.start({ port: 4001 }, () => console.log(`Server is running on http://localhost:4001`));
+    await createConnections();
+    // await createConnection('test')
+    await graphQLServer.start({ port }, () => console.log(`Server is running on http://localhost:` + port));
   });
+
+  after(() => getConnection('test').close());
+
   describe('Login', () => {
     let request: supertest.SuperTest<supertest.Test>;
+    let userRepository: Repository<User>;
     const testUser = {
       name: "test",
       email: "test-email@example.com",
@@ -24,21 +30,19 @@ describe('GraphQL', () => {
       birthDate: "01-01-1970",
       cpf: 28
     };
-
     const wrongCredentials = {
       email: testUser.email.split("").reverse().join(""),
       password: testUser.password.split("").reverse().join("")
-    }
+    };
 
     before(() => {
       request = supertest(url);
-
-      const userRepository = getRepository(User);
+      userRepository = getRepository(User, 'test');
+      
       userRepository.save(testUser);
     });
 
     after(async () => {
-      const userRepository = getRepository(User);
       const users = await userRepository.find({
         where: {
           name: testUser.name,
@@ -50,7 +54,7 @@ describe('GraphQL', () => {
       });
       const user = users[users.length - 1];
       userRepository.delete(user);
-    })
+    });
 
     it(`Successfully returns a valid token for user with correct credentials`, (done) => {
       request.post('/')
@@ -71,7 +75,7 @@ describe('GraphQL', () => {
           });
           assert.equal(verification.id, res.body.data.login.user.id, 'Token does not match user information');
           done();
-        })
+        });
     });
 
     it(`Successfully returns the right user for the correct credentials`, (done) => {
@@ -96,7 +100,7 @@ describe('GraphQL', () => {
           assert.equal(returnedUser.birthDate, testUser.birthDate, 'Wrong birthDate return from database');
           assert.equal(returnedUser.cpf, testUser.cpf, 'Wrong cpf return from database');
           done();
-        })
+        });
     });
 
     it(`Fails logging in for an existent email with a wrong password`, (done) => {
@@ -109,7 +113,7 @@ describe('GraphQL', () => {
           if (err) return err;
           assert(res.body.errors, 'Error expected');
           done();
-        })
+        });
     });
 
     it(`Fails logging in for an unexistent email and an existent password`, (done) => {
@@ -122,7 +126,7 @@ describe('GraphQL', () => {
           if (err) return err;
           assert(res.body.errors, 'Error expected');
           done();
-        })
+        });
     });
   });
 });
